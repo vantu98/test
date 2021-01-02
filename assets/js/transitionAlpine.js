0d3189df532b6466cdb48c8223e77489fce12049
tunv <nguyenvantu191298@gmail.com>
2021-01-02 14:21:52 +0700

    show/hide bottom menu

--- assets/js/transitionAlpine.js
+++ assets/js/transitionAlpine.js
@@ -1,16 +1,24 @@
 function transitionAlpine() {
   return {
+    //vars
     isSidebarOpen: false,
     isCartOpen: false,
+    showBottomMenu: true,
+    // Sidebar
     sidebarOpen() {
       this.isSidebarOpen = true;
-      console.log("open sidebar");
+      // console.log("open sidebar");
     },
     sidebarClose() {
       this.isSidebarOpen = false;
     },
     isSideBarOpenFunc() {
       return this.isSidebarOpen === true;
+    },
+    // Bottom Menu
+    isScrollAtTop() {
+      let isScrollAtTop = window.pageYOffset > 50 ? true : false;
+      this.showBottomMenu = !isScrollAtTop;
     },
   };
 }
