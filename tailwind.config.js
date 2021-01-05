0f9103fa006ba0b364f4ab83371172aff2a31020
tunv <nguyenvantu191298@gmail.com>
2021-01-05 23:49:02 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       fontFamily: {
         "primary-font-sans": ["Source Sans Pro"],
+        "primary-serif": ["Playfair Display"],
       },
       zIndex: {
         "-1": "-1",
@@ -12,6 +13,11 @@ module.exports = {
       transformOrigin: {
         0: "0%",
       },
+      minHeight:{
+        '120px': '120px',
+        '135px': '135px',
+        '143px': '143px'
+      }
     },
   },
   variants: {
