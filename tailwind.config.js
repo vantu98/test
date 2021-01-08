36557a06eeec54f0cccdc231e91a6d65cb30345e
tunv <nguyenvantu191298@gmail.com>
2021-01-08 18:49:15 +0700

    homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,60 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "primary-font-sans": ["Source Sans Pro"],
         "primary-serif": ["Playfair Display"],
       },
-      zIndex: {
-        "-1": "-1",
+      minHeight: {
+        "40px": "40px",
+        "120px": "120px",
+        "135px": "135px",
+        "143px": "143px",
       },
-      transformOrigin: {
-        0: "0%",
+      backgroundColor: {
+        "banner-red": "#F5E3E7",
+        "section-red": "#FAF2F4",
+        "red-section": "#FAF2F4",
+        "gray-item": "#FAFAFA",
       },
-      minHeight:{
-        '120px': '120px',
-        '135px': '135px',
-        '143px': '143px'
-      }
+      textColor: {
+        "hover-purple": "#5858FF",
+        "gray-sub": "#555555",
+      },
+      fontSize: {
+        "11px": "11px",
+      },
+      lineHeight: {
+        "11px": "11px",
+        "14px": "14px",
+      },
+      letterSpacing: {
+        "1px": "1px",
+      },
+      height: {
+        "19px": "19px",
+      },
+      borderRadius: {
+        "3px": "3px",
+      },
     },
+    container: {
+      center: true,
+      padding: {
+        sm: "16px",
+        md: "36px",
+        lg: "48px",
+        xl: "56px",
+      },
+    },
+    // screens: {
+    //   sm: "640px",
+    //   md: "768px",
+    //   lg: "1024px",
+    //   xl: "1152px",
+    //   "2xl": "1536px",
+    // },
   },
   variants: {
     extend: {},
-    borderColor: ["focus-within", "hover"],
   },
   plugins: [],
 };
